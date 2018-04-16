4dd1bf15ee3b459678dada6083553d2e651fcce9
Antonis Antoniou <kountouris7@gmail.com>
2018-04-16 23:29:32 +0300

    Lrcst 22-user has profile

--- app/Channel.php
+++ app/Channel.php
@@ -1,18 +1,24 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Channel extends Model
 {
+    /**
+     * Get the route key name for Laravel.
+     *
+     * @return string
+     */
     public function getRouteKeyName()
     {
         return 'slug';
     }
-public function threads()
-{
-    return $this->hasMany(Thread::class);
-}
-
+    /**
+     * A channel consists of threads.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -15,7 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -24,6 +26,28 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    /**
+     * Get the route key name for Laravel.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
+
+    /**
+     * Fetch all threads that were created by the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function threads()
+    {
+        return $this->hasMany(Thread::class)->latest();
+    }
 }
+
